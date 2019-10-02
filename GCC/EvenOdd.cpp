918bcb37f62e30ed7d01854e593ddf293f8a2023
Akanksha Singh <akankshasingh121297@gmail.com>
2019-10-02 19:07:32 +0530

    Check even odd (#51)
    * Check even odd

---
+++ GCC/EvenOdd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter a number :";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"This is an even number";
+	}
+	else
+	{
+		cout<<"This is an odd number";
+	}
+    return 0;
+}
